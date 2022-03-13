85c67b8b05bb12350cde9a189b9e7b0fdd66d967
Dalu Cheng <lcheng@maplesoft.com>
2022-03-13 17:59:24 -0400

    finish review

--- assignment2/move_recommendation.py
+++ assignment2/move_recommendation.py
@@ -1,9 +1,16 @@
+"""
+Assignment 2 question 3
+
+Implementation is adapted from the code provided.
+"""
+
 import numpy as np
 
 # 7 MOVIES: Legally Blond; Matrix; Bourne Identity; You’ve Got Mail;
 # The Devil Wears Prada; The Dark Knight; The Lord of the Rings.
 # 9 users.
 # preference matrix. Each row is a user's preference for movies.
+# 1 means like, 0 means no rating, -1 means dislike.
 P = np.array([[0, 0, -1, 0, -1, 1, 1],
               [-1, 1, 1, -1, 0, 1, 1],
               [0, 1, 1, 0, 0, -1, 1],
@@ -15,15 +22,15 @@ P = np.array([[0, 0, -1, 0, -1, 1, 1],
               [0, 0, -1, 1, 1, 0, -1]])
 
 # Parameters
-reg = 0.1 # regularization parameter
+reg = 0.1  # regularization parameter
 f = 2  # number of factors. (embedding has 2 dimensions)
-m, n = P.shape
+m, n = P.shape  # m users, n movies
 
-# Random Initialization. rand() samples from a uniform distribution over (0, 1)
-X = 1 - 2 * np.random.rand(m, f)  # X is (m x f) in (-1, 1) range
-Y = 1 - 2 * np.random.rand(f, n)  # Y is (f x n) in (-1, 1) range
-X *= 0.1
-Y *= 0.1
+# Random Initialization.
+X = 1 - 2 * np.random.rand(m, f)
+Y = 1 - 2 * np.random.rand(f, n)
+X *= 0.1  # X ~ Uniform(-0.1, 0.1)
+Y *= 0.1  # Y ~ Uniform(-0.1, 0.1)
 
 # Alternating Ridge Regression
 for _ in range(100):
@@ -37,6 +44,7 @@ X = 1 - 2 * np.random.rand(m, f)
 Y = 1 - 2 * np.random.rand(f, n)
 X *= 0.1
 Y *= 0.1
+
 # Alternating Weighted Ridge Regression
 C = np.abs(P)  # Will be 0 only when P[i,j] == 0.
 for _ in range(100):
@@ -44,12 +52,18 @@ for _ in range(100):
     for u, Cu in enumerate(C):
         Cu_diag = np.diag(Cu)
         A = Y @ Cu_diag @ Y.T + reg * np.eye(f)
-        v = Y @ Cu_diag @ P[u]
+        v = np.dot(Y, np.dot(Cu_diag, P[u]))
         X[u] = np.linalg.solve(A, v).T  # solve Ax[u]=v by avoiding matrix inverse computation
     for i, Ci in enumerate(C.T):
         Ci_diag = np.diag(Ci)
         A = X.T @ Ci_diag @ X + reg * np.eye(f)
-        v = X.T @ Ci_diag @ P[:, i]
+        v = np.dot(X.T, np.dot(Ci_diag, P[:, i]))
         Y[:, i] = np.linalg.solve(A, v)
 print('Alternating Weighted Ridge Regression:')
-print(np.dot(X, Y))
+matches = np.dot(X, Y)
+print(matches)
+
+movies = np.array(['Legally Blond', 'Matrix', 'Bourne Identity', 'You’ve Got Mail',
+                   'The Devil Wears Prada', 'The Dark Knight', 'The Lord of the Rings'])
+recommend = movies[np.argmax(matches, axis=1)]  # top movie recommended for each user
+print(recommend)
